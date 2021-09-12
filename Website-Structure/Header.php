77d04c82e051237243882e8bc42b87ce4f6e4a63
AbdulBaset R. Sayed <74756037+AbdulBasetRS@users.noreply.github.com>
2021-09-12 09:10:25 +0200

    update header form login

--- Website-Structure/Header.php
+++ Website-Structure/Header.php
@@ -1,168 +1,168 @@
 <!-- bob -->
-  <div class="background-when-menu"></div>
-  <div class="Login-username Login-dissapered">
-    <form class="Login-user">
-      <div class="cancel login-cancel">
-        <i class="fas fa-times"></i>
-      </div>
-      <h2>Login</h2>
-      <div class="form-group">
-        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Username">
-      </div>
-      <div class="form-group">
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-      </div>
-      <div class="remember-me">
-      <div class="cheack">
-          <div>
-            <input type="checkbox" class="" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Remember me</label>
-          </div>
-      </div>
-      <div class="lost-password">
-          <a href = "#"> Lost your password? </a>
-      </div>
-      </div>
-      <button type="submit" class="btn form-control submit-user">Log in</button>
-      <div class="regester">
-        <span> Not a member </span> <a href="#">Register</a>
-      </div>
-    </form>
-  </div>
-
-  <div class="bob-search bob-search-remove">
-      <div class="cancel_search-cancel">
+<div class="background-when-menu"></div>
+<div class="Login-username Login-dissapered">
+    <form class="Login-user" action="" method="POST">
+        <div class="cancel login-cancel">
             <i class="fas fa-times"></i>
-      </div>
-      <form class="search-input">
+        </div>
+        <h2>Login</h2>
+        <div class="form-group">
+            <input type="text" name="Input_Username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Username">
+        </div>
+        <div class="form-group">
+            <input type="password" name="Input_Password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <div class="remember-me">
+            <div class="cheack">
+                <div>
+                    <input type="checkbox" class="" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Remember me</label>
+                </div>
+            </div>
+            <div class="lost-password">
+                <a href="#"> Lost your password? </a>
+            </div>
+        </div>
+        <button type="submit" name="BTN_Login" class="btn form-control submit-user">Log in</button>
+        <div class="regester">
+            <span> Not a member </span> <a href="#">Register</a>
+        </div>
+    </form>
+</div>
+
+<div class="bob-search bob-search-remove">
+    <div class="cancel_search-cancel">
+        <i class="fas fa-times"></i>
+    </div>
+    <form class="search-input">
         <input type="search" placeholder="Search Products" />
-        <select class="select-search" >
+        <select class="select-search">
             <option>All Categories</option>
             <option>Men</option>
             <option>Speakers</option>
             <option>Women</option>
         </select>
-      </form>
-  </div>
-  <div class="with-search with-search-remove"></div>
+    </form>
+</div>
+<div class="with-search with-search-remove"></div>
 <!-- bob -->
 
 <!-- nav -->
 <nav class="navbar navbar-expand-lg navbar-light scroll">
-  <div class="menu-burger">
-      <i class="fas fa-align-left"></i>
-  </div>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About Us</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Shop</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contact Us</a>
-      </li>
-    </ul>
-  </div>
-  <div class="logo-brand ">
-      <img src="IMG/brand-logo.png" alt="brand-logo"/>
-  </div>
-  <div class="icones-items items-Desktop">
-    <a href="#" class="login">Login</a>
-    <a href="#" class="search-nav"><i class="fas fa-search"></i></a>
-    <a href="#"><i class="far fa-heart"></i></a>
-    <a href="#"><i class="fas fa-cart-plus"></i></a>
-  </div>
-  <div class="icones-items items-mobile">
-    <a href="#"><i class="fas fa-cart-plus"></i></a>
-  </div>
-</nav>
-<div class="left-menu">
-  <div class="cancel">
-    <i class="fas fa-times"></i>
-  </div>
-  <div class = "content-left-menu">
-    <div class="search-for-products">
-      <form class="search-input">
-        <input type="search" placeholder="Search Products" />
-      </form>
+    <div class="menu-burger">
+        <i class="fas fa-align-left"></i>
     </div>
-    <ul class="navbar-menu-burger">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About Us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Shop</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact Us</a>
-        </li>
-      </ul>
-      <ul class="navbar-menu-burger anthoer-navbar-menu-burger Login-menu-burger">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Login</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#"><i class="far fa-user"></i></a>
-        </li>
-      </ul>
-      <ul class="navbar-menu-burger anthoer-navbar-menu-burger last-navbar-menu-burger">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Cart</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#"><i class="fas fa-cart-plus"></i></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Wishlist</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#"><i class="far fa-heart"></i></a>
-        </li>
-      </ul>
-      <ul class="navbar-menu-burger anthoer-navbar-menu-burger">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Language</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">En</a>
-        </li>
-      </ul>
-      <div class="social-media">
-        <ul class="social-media">
-            <li>
-              <a href="#"><i class="fab fa-facebook-f"></i></a>
-              <a href="#"><i class="fab fa-instagram"></i></a>
-              <a href="#"><i class="fab fa-twitter"></i></a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Shop</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact Us</a>
             </li>
         </ul>
-      </div>
-  </div>
+    </div>
+    <div class="logo-brand ">
+        <img src="IMG/brand-logo.png" alt="brand-logo" />
+    </div>
+    <div class="icones-items items-Desktop">
+        <a href="#" class="login">Login</a>
+        <a href="#" class="search-nav"><i class="fas fa-search"></i></a>
+        <a href="#"><i class="far fa-heart"></i></a>
+        <a href="#"><i class="fas fa-cart-plus"></i></a>
+    </div>
+    <div class="icones-items items-mobile">
+        <a href="#"><i class="fas fa-cart-plus"></i></a>
+    </div>
+</nav>
+<div class="left-menu">
+    <div class="cancel">
+        <i class="fas fa-times"></i>
+    </div>
+    <div class="content-left-menu">
+        <div class="search-for-products">
+            <form class="search-input">
+                <input type="search" placeholder="Search Products" />
+            </form>
+        </div>
+        <ul class="navbar-menu-burger">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Shop</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact Us</a>
+            </li>
+        </ul>
+        <ul class="navbar-menu-burger anthoer-navbar-menu-burger Login-menu-burger">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="far fa-user"></i></a>
+            </li>
+        </ul>
+        <ul class="navbar-menu-burger anthoer-navbar-menu-burger last-navbar-menu-burger">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Cart</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fas fa-cart-plus"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Wishlist</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="far fa-heart"></i></a>
+            </li>
+        </ul>
+        <ul class="navbar-menu-burger anthoer-navbar-menu-burger">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Language</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">En</a>
+            </li>
+        </ul>
+        <div class="social-media">
+            <ul class="social-media">
+                <li>
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </div>
 <!-- nav -->
 <div id="carouselExampleControlsNoTouching" class="carousel slide" data-touch="false" data-interval="false">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="IMG/ezgif.com-gif-maker.jpg" class="d-block w-100" alt="ezgif.com-gif-maker.jpg">
-      
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="IMG/ezgif.com-gif-maker.jpg" class="d-block w-100" alt="ezgif.com-gif-maker.jpg">
+
+        </div>
+        <div class="carousel-item">
+            <img src="IMG/ezgif.com-gif-maker (1).jpg" class="d-block w-100" alt="ezgif.com-gif-maker (1).jpg">
+        </div>
     </div>
-    <div class="carousel-item">
-      <img src="IMG/ezgif.com-gif-maker (1).jpg" class="d-block w-100" alt="ezgif.com-gif-maker (1).jpg">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControlsNoTouching" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControlsNoTouching" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+    <a class="carousel-control-prev" href="#carouselExampleControlsNoTouching" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControlsNoTouching" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
 </div>
