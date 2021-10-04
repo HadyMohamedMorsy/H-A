2547bff0bbbd9aad54e2dd657da777389cc1bc95
AbdulBaset R. Sayed <74756037+AbdulBasetRS@users.noreply.github.com>
2021-10-04 06:27:03 +0200

    Update

--- Datebase/DB_Products_List.php
+++ Datebase/DB_Products_List.php
@@ -3,7 +3,12 @@
     $Current_Time = date('h:i:s A');
     $Current_Date_And_Time = $Current_Date . ' ' . $Current_Time;
     $Alert_Message = array();
+    // Product Info..
     $SQL_Products_List = 'SELECT * FROM ha_products WHERE HA_P_Status = "Active" OR HA_P_Status = "Pending" ORDER BY HA_P_ID DESC ';
     $Result_Products_List = mysqli_query($Connection,$SQL_Products_List);
     $Count_Products_List  = mysqli_num_rows($Result_Products_List);
+    // Category List
+    $SQL_Category_List = 'SELECT * FROM ha_category_list WHERE HA_C_L_Status = "Active" AND HA_C_L_Parent_ID != "" ORDER BY HA_C_L_ID DESC ';
+    $Result_Category_List = mysqli_query($Connection,$SQL_Category_List);
+    $Count_Category_List  = mysqli_num_rows($Result_Category_List);
 ?>
--- Website-Structure/Products-List-Body.php
+++ Website-Structure/Products-List-Body.php
@@ -1,27 +1,27 @@
 <div class="right-All-Clients Dasbored-content">
-<table id="example" class="table table-striped table-bordered" style="width:100%;text-align: center;">
-        <thead >
+    <table id="example" class="table table-striped table-bordered" style="width:100%;text-align: center;">
+        <thead>
             <tr>
                 <th>ID</th>
                 <th>IMG</th>
-                <th style="display: none;"></th>
-                <th style="display: none;"></th>
-                <th style="display: none;"></th>
-                <th style="display: none;"></th>
-                <th style="display: none;"></th>
-                <th style="display: none;"></th>
-
                 <th>Name</th>
                 <th>Catagroy</th>
                 <th>Qty</th>
                 <th>Price</th>
                 <th>Status</th>
+                <th style="display: none;">ID Category</th>
+                <th style="display: none;">Alert Qty</th>
+                <th style="display: none;">Brand</th>
+                <th style="display: none;">Available From Date</th>
+                <th style="display: none;">Date And Time Created</th>
+                <th style="display: none;">User Created</th>
+                <th style="display: none;">Description</th>
                 <th>Action</th>
             </tr>
         </thead>
-        <tbody id="tbody"> 
+        <tbody id="tbody">
             <!-- ImgCover -->
-                <?php
+            <?php
                     if ($Count_Products_List > 0) {
                         while ($Rows = mysqli_fetch_array($Result_Products_List)) {
                             $SQL_Get_Category_Name = 'SELECT HA_C_L_Category_Name FROM ha_category_list WHERE HA_C_L_ID = "'.$Rows['HA_P_Category_ID'].'"';
@@ -36,22 +36,23 @@
                             echo '
                                 <tr class="t-body" >
                                     <td>'.$Rows['HA_P_ID'].'</td>
+                                    <td class="IMG-DATABASE"><img src="'.$Path_Folder_Cover . '/' . $Cover_Img[2] . '" alt="Img" style="width:50px"/></td>
+                                    <td class="Username-Edit-Module">'.$Rows['HA_P_Name'].'</td>
+                                    <td >'.$Row_Get_Category_Name['HA_C_L_Category_Name'].'</td>
+                                    <td class="HA-P-Qty">'.$Rows['HA_P_Qty'].'</td>
+                                    <td class="HA-P-Price">'. number_format($Rows['HA_P_Price'] , 2).'</td>
+                                    <td class="HA-P-Status">'.$Rows['HA_P_Status'].'</td>
+                                    <td style="display:none" class="HA-C-L-Category-Name">'.$Rows['HA_P_Category_ID'].'</td>
                                     <td style="display:none">'.$Rows['HA_P_Alert_Qty'].'</td>
                                     <td style="display:none">'.$Rows['HA_P_Brand'].'</td>
                                     <td style="display:none">'.$Rows['HA_P_Available_From_Date'].'</td>
                                     <td style="display:none">'.$Rows['HA_P_Date_Created'] . ' ' . $Rows['HA_P_Time_Created'] .'</td>
                                     <td style="display:none">'.$Row_Get_Created_Info['HA_U_Username'].'</td>
                                     <td style="display:none">'.$Rows['HA_P_Description'].'</td>
-                                    <td class="IMG-DATABASE"><img src="'.$Path_Folder_Cover . '/' . $Cover_Img[2] . '" alt="Img" style="width:50px"/></td>
-                                    <td class="Username-Edit-Module">'.$Rows['HA_P_Name'].'</td>
-                                    <td class="HA-C-L-Category-Name">'.$Row_Get_Category_Name['HA_C_L_Category_Name'].'</td>
-                                    <td class="HA-P-Qty">'.$Rows['HA_P_Qty'].'</td>
-                                    <td class="HA-P-Price">'. number_format($Rows['HA_P_Price'] , 2).'</td>
-                                    <td class="HA-P-Status">'.$Rows['HA_P_Status'].'</td>
                                     <td class="last-Action">
-                                    <a href="#" class="Trash" data-module=".question-delete-row"> <i class="fas fa-trash-alt"></i> </a>
-                                    <a href="#" class="Edit" data-module=".Edit-row"> <i class="far fa-edit"></i> </a>
-                                    <a href="#" class="View" data-module=".View-row"> <i class="far fa-eye"></i> </a>
+                                        <a href="#" class="Trash" data-module=".question-delete-row"> <i class="fas fa-trash-alt"></i> </a>
+                                        <a href="#" class="Edit" data-module=".Edit-row"> <i class="far fa-edit"></i> </a>
+                                        <a href="#" class="View" data-module=".View-row"> <i class="far fa-eye"></i> </a>
                                     </td>
                                 </tr>
                             ';
@@ -63,17 +64,18 @@
             <tr>
                 <th>ID</th>
                 <th>IMG</th>
-                <th style="display: none;"></th>
-                <th style="display: none;"></th>
-                <th style="display: none;"></th>
-                <th style="display: none;"></th>
-                <th style="display: none;"></th>
-                <th style="display: none;"></th>
                 <th>Name</th>
                 <th>Catagroy</th>
                 <th>Qty</th>
                 <th>Price</th>
                 <th>Status</th>
+                <th style="display: none;">ID Category</th>
+                <th style="display: none;">Alert Qty</th>
+                <th style="display: none;">Brand</th>
+                <th style="display: none;">Available From Date</th>
+                <th style="display: none;">Date And Time Created</th>
+                <th style="display: none;">User Created</th>
+                <th style="display: none;">Description</th>
                 <th>Action</th>
             </tr>
         </tfoot>
@@ -90,26 +92,26 @@
         <div class="View-row-Details">
             <div class="View-cancel cancel-Dashbored"> <i class="fas fa-times"></i> </div>
             <div class="Image-View">
-                <img  alt="IMG/emp_default.jpg" id="view-Details-show-IMG"/>
+                <img alt="IMG/emp_default.jpg" id="view-Details-show-IMG" />
             </div>
             <div class="Deatils-View">
                 <ul>
-                    <li>Name         : <span id="view-Details-show-name"></span></li>
-                    <li>Catagroy     : <span id="view-Details-show-Catagroy"></span></li>
-                    <li>QTY          : <span id="view-Details-show-Qty"></span></li>
-                    <li>Price        : <span id="view-Details-show-Date-Price"></span></li>
-                    <li>Status       : <span id="view-Details-show-Status"></span></li>
+                    <li>Name : <span id="view-Details-show-name"></span></li>
+                    <li>Catagroy : <span id="view-Details-show-Catagroy"></span></li>
+                    <li>QTY : <span id="view-Details-show-Qty"></span></li>
+                    <li>Price : <span id="view-Details-show-Date-Price"></span></li>
+                    <li>Status : <span id="view-Details-show-Status"></span></li>
                 </ul>
             </div>
         </div>
     </div>
 
     <div class="Edit-row remove-Delete">
-    <form class="Edit-row-details" action="" method="POST">
+        <form class="Edit-row-details" action="" method="POST">
             <div class="cancel-Dashbored Edit-cancel">
                 <i class="fas fa-times"></i>
             </div>
-            <h2>Edit Profile</h2>
+            <h2>Edit Product</h2>
             <div class="form-group">
                 <input type="number" name="Input_Id" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Username" hidden>
             </div>
@@ -117,22 +119,28 @@
                 <input type="text" name="Input_Username" class="form-control" id="Edit-Username" aria-describedby="emailHelp" placeholder="Username">
             </div>
             <div class="form-group">
-            <select name="Input-Status" class="form-control" id="Catagroy-Name">
-                    <option value="">car</option>
-                    <option value="TV">TV</option>
-                    <option value="watch">watch</option>
-             </select>
+                <select name="Input-Status" class="form-control" id="Catagroy-Name">
+                    <?php
+                        if ($Count_Category_List > 0) {
+                            while ($Rows = mysqli_fetch_array($Result_Category_List)) {
+                                $SQL_Get_Parent_Name = 'SELECT HA_C_L_Category_Name FROM ha_category_list WHERE HA_C_L_ID = "'.$Rows['HA_C_L_Parent_ID'].'"';
+                                $Result_Get_Parent_Name = mysqli_query($Connection,$SQL_Get_Parent_Name);
+                                $Row_Get_Parent_Name  = mysqli_fetch_array($Result_Get_Parent_Name, MYSQLI_ASSOC);
+                                echo '<option value="'.$Rows['HA_C_L_ID'].'"> '.$Row_Get_Parent_Name['HA_C_L_Category_Name'] . ' | ' . $Rows['HA_C_L_Category_Name'] .'</option>';
+                            }
+                        }
+                    ?>
+                </select>
             </div>
             <div class="form-group">
-              <select name="Input-Status" class="form-control" id="Status">
-                    <option value="">Status</option>
+                <select name="Input-Status" class="form-control" id="Status">
                     <option value="Active">Active</option>
-                    <option value="UnActice">UnActice</option>
+                    <option value="Deactivate">Deactivate </option>
                     <option value="Pending">Pending</option>
                     <option value="Suspended">Suspended</option>
-             </select>
+                </select>
             </div>
             <button type="submit" name="BTN_Register" class="btn form-control Edit-Prfile-user">Edit</button>
         </form>
-     </div>
+    </div>
 </div>
