e579d09c932b68ac46b40c6fa328d24610ec8a66
hadymohamedmohamedmorsy <hady812012@gmai.com>
2021-10-13 02:10:01 +0200

    Add

--- Links javascript Dashbored.php
+++ Links javascript Dashbored.php
@@ -1,6 +1,8 @@
 <script src="Framework/jQuery/uncompressed, development jQuery 3.5.1.js"></script>
-<script src="./Framework/Bootstrap/js/datatables.js"></script>
+<script src="./Framework/Bootstrap/js/dataTables.min.js"></script>
+<script src="./Framework/Bootstrap/js/dataTables.bootstrap4.min.js"></script>
 <script src="Framework/Bootstrap/js/bootstrap.js"></script>
-<script src="./Framework/Bootstrap/js/datablesnet.js"></script>
 <script src="Framework/Fontawsome/js/all.js"></script>
 <script src="Javascript-Files/Dashbored-index-Page.js"></script>
+
+
