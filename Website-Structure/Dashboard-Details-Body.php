3b77db8c58c36caee3fa9c1a0409fe2e74b7a296
hadymohamedmohamedmorsy <hady812012@gmai.com>
2021-10-15 01:46:58 +0200

    Add

--- Website-Structure/Dashboard-Details-Body.php
+++ Website-Structure/Dashboard-Details-Body.php
@@ -7,10 +7,50 @@
 
         <div class="analysis">
             <div class="row">
-                    <div class="col-lg-3"><div class="analysis-chiled">Hello</div></div>
-                    <div class="col-lg-3"><div class="analysis-chiled">Hello</div></div>
-                    <div class="col-lg-3"><div class="analysis-chiled">Hello</div> </div>
-                    <div class="col-lg-3"><div class="analysis-chiled">Hello</div></div>
+                <div class="col-lg-3">
+                    <div class="analysis-chiled">
+                        <div class="anaysis-icon Revenue">
+                        <i class="fas fa-dollar-sign"></i>
+                        </div>
+                        <div class="anaysis-details ">
+                            <h4>Revenue</h4>
+                            <span class="number"> 13,456.5</span>
+                        </div>  
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="analysis-chiled">
+                        <div class="anaysis-icon Orders">
+                        <i class="fas fa-truck"></i>
+                        </div>
+                        <div class="anaysis-details">
+                        <h4>Orders</h4>
+                        <span class="number"> 13,456.5</span>
+                        </div>  
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="analysis-chiled">
+                        <div class="anaysis-icon Products">
+                        <i class="fas fa-box"></i>
+                        </div>
+                        <div class="anaysis-details">
+                        <h4>Products</h4>
+                        <span class="number "> 13,456.5</span>
+                        </div>  
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="analysis-chiled">
+                        <div class="anaysis-icon Monthly">
+                        <i class="fas fa-briefcase"></i>
+                        </div>
+                        <div class="anaysis-details">
+                        <h4>Monthly Earning</h4>
+                        <span class="number "> 13,456.5</span>
+                        </div>  
+                    </div>
+                </div>
             </div>
         </div>
 
