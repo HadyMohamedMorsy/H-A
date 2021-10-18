b81d4f11bd79d75dcddfdc0cfada8b9886cd1b0e
AbdulBaset R. Sayed <74756037+AbdulBasetRS@users.noreply.github.com>
2021-10-18 13:00:41 +0200

    Update

--- Datebase/DB-Vendor-Clients-Manger.php
+++ Datebase/DB-Vendor-Clients-Manger.php
@@ -3,7 +3,7 @@
     $Current_Time = date('h:i:s A');
     $Current_Date_And_Time = $Current_Date . ' ' . $Current_Time;
     $Alert_Message = array();
-    $SQL_Clients_Info = 'SELECT * FROM ha_users WHERE HA_U_User_Type = "Client" ORDER BY HA_U_ID DESC ';
+    $SQL_Clients_Info = 'SELECT * FROM ha_users WHERE HA_U_User_Type = "Client" AND HA_U_User_Status != "Deactivated" ORDER BY HA_U_ID DESC ';
     $Result_Clients_Info = mysqli_query($Connection,$SQL_Clients_Info);
     $Count_Clients_Info  = mysqli_num_rows($Result_Clients_Info);
     if (isset($_POST['BTN_Active'])) {
