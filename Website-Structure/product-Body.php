0fd174565e2a3e3e2f28a97fa5ede40595222fd7
hadymohamedmohamedmorsy <hady812012@gmai.com>
2021-10-21 20:13:27 +0200

    ADD

--- Website-Structure/product-Body.php
+++ Website-Structure/product-Body.php
@@ -1,17 +1,64 @@
 <!-- Header -->
     <div class="header-pages-Product">
-        
     </div>
-
 <!-- header -->
 <div class="container-fluid Secound-Section">
         <div class="icones-filter">
-          <div class="icones-include-filter">  <i class="fas fa-filter"></i> <span> Filters</span>  </div>
-          <div class="icones-include-filter">  <span> Filters</span>  </div>
-          <div class="icones-include-filter">  <i class="fas fa-filter"></i> <span> Filters</span>  </div>
-          <div class="icones-include-filter">  <i class="fas fa-filter"></i> <span> Filters</span>  </div>
-          <div class="icones-include-filter">  <i class="fas fa-filter"></i> <span> Filters</span>  </div>
+          <div class="icones-include-filter"> <div class="filter"> <span class="filter-trigger"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sliders"><line x1="4" y1="21" x2="4" y2="14"></line><line x1="4" y1="10" x2="4" y2="3"></line><line x1="12" y1="21" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="3"></line><line x1="20" y1="21" x2="20" y2="16"></line><line x1="20" y1="12" x2="20" y2="3"></line><line x1="1" y1="14" x2="7" y2="14"></line><line x1="9" y1="8" x2="15" y2="8"></line><line x1="17" y1="16" x2="23" y2="16"></line></svg> </span> <span> Filters</span></div></div>
+          <div class="icones-include-filter">
+            <form class="woocommerce-ordering" method="get">
+              <select name="orderby" class="orderby" aria-label="Shop order">
+                  <option value="menu_order" selected="selected">Default sorting</option>
+                  <option value="popularity">Sort by popularity</option>
+                  <option value="rating">Sort by average rating</option>
+                  <option value="date">Sort by latest</option>
+                  <option value="price">Sort by price: low to high</option>
+                  <option value="price-desc">Sort by price: high to low</option>
+              </select>
+            </form>
+          </div>
+          <div class=" shop-views list-1 small-1">
+							<button id="shop-display-grid" class="shop-display grid-icon active" data-display="grid">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-grid"><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
+              </button>
+							<button id="shop-display-small" class="shop-display small-icon" data-display="small">
+								<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path d="M20.016 8.016v-4.031h-4.031v4.031h4.031zM20.016 14.016v-4.031h-4.031v4.031h4.031zM20.016 20.016v-4.031h-4.031v4.031h4.031zM14.016 8.016v-4.031h-4.031v4.031h4.031zM14.016 14.016v-4.031h-4.031v4.031h4.031zM14.016 20.016v-4.031h-4.031v4.031h4.031zM8.016 8.016v-4.031h-4.031v4.031h4.031zM8.016 14.016v-4.031h-4.031v4.031h4.031zM8.016 20.016v-4.031h-4.031v4.031h4.031zM20.016 2.016q0.797 0 1.383 0.586t0.586 1.383v16.031q0 0.797-0.586 1.383t-1.383 0.586h-16.031q-0.797 0-1.383-0.586t-0.586-1.383v-16.031q0-0.797 0.586-1.383t1.383-0.586h16.031z"></path>
+                </svg>
+              </button>
+							<button id="shop-display-list" class="shop-display list-icon" data-display="list">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
+              </button>
+						</div>
         </div>
     <div class="row">
     </div>
+    <div class="button-load">
+				<a href="#" class="et-infload-btn et-shop-infload-btn button outlined">Load More</a>
+		</div>
 </div>
+  <div class="click-filter">
+    <div class="panel-open-filters">
+        <header>
+            <div class="side-panel-title">Filters</div>
+            <a href="#" class="et-close button btn-sm" title="Done">Done</a>
+        </header>
+        <div class="side-panel-content">
+          <ul class="side-panel-content-list">
+            <div id="wcapf-active-filters-1" class="wcapf-widget-hidden woocommerce wcapf-ajax-term-filter widget cf widget_wcapf-active-filters">
+              <h2 class="widgettitle collapsible">
+              FILTER BY CATEGORY
+                <span>
+                  <i class="fas fa-angle-up"></i>
+                </span>
+              </h2>
+            </div>
+            <ul class="catagroy-filter">
+              <li>Men</li>
+              <li>Speaker</li>
+              <li>women</li>
+            </ul>
+          </ul>
+        </div>
+    </div>
+  </div>
