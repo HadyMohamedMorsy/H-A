0263c0dfe49d221beed5a10e4399dda9bc01d48d
hadymohamedmohamedmorsy <hady812012@gmai.com>
2021-10-24 01:35:26 +0200

    add

---
+++ Website-Structure/product-show.php
@@ -0,0 +1,4 @@
+<div class="slider-product">
+    <div class="overlay"></div>
+    <img src="./IMG/21.jpg" alt="21.jpg"/>
+</div>
---
+++ products-show.php
@@ -0,0 +1,35 @@
+<?php
+    include('Datebase/Config.php');
+    include('Datebase/DB_Login.php');
+    include('Datebase/DB-Sign-Up.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+    $Page_Title = 'H_A';
+    include ('Website-Structure/Header-Elemnts.php') ;
+?>
+    <!-- Start The Links Files -->
+    <?php include ("Links CSS In Head Area.php"); ?>
+    <!-- End The Links Files -->
+<body>
+
+    <!-- Start The Header -->
+    <?php include ("Website-Structure/Header.php"); ?>
+    <!-- End The Header-->
+
+    <?php include ("Website-Structure/product-show.php"); ?>
+
+    <!-- Start The Footer -->
+    <?php include ("Website-Structure/Footer.php"); ?>
+    <!-- End The Footer-->
+
+    <!-- Start The Links Files -->
+    <?php include ("Links Javascript In Footer Area.php"); ?>
+    <!-- <script type="text/javascript" src="Javascript-Files/Main-index-Page.js"></script> -->
+    <!-- End The Links Files -->
+</body>
+
+</html>
