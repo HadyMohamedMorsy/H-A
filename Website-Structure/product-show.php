b06073988bd39ab0d768bba6552173fb49f6ac0a
hadymohamedmohamedmorsy <hady812012@gmai.com>
2021-10-24 20:40:15 +0200

    add new

--- Website-Structure/product-show.php
+++ Website-Structure/product-show.php
@@ -1,4 +1,115 @@
 <div class="slider-product">
     <div class="overlay"></div>
     <img src="./IMG/21.jpg" alt="21.jpg"/>
+    <div class="right-arrow">  <i class="fas fa-chevron-right"></i> </div>
+    <div class="left-arrow">  <i class="fas fa-chevron-left"></i> </div>
+    <div class="slider-img">
+        <div class="img-slider"> <img src="./IMG/5.jpg" alt="5" /> </div>
+        <div class="img-slider"> <img src="./IMG/6.jpg" alt="6" /> </div>
+        <div class="img-slider"> <img src="./IMG/7.jpg" alt="7" /> </div>
+    </div>
+</div>
+<div class="content-description">
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-12 Desc">
+                <h2> Description </h2>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum perspiciatis possimus illum, vitae, maiores officia impedit ipsum consectetur earum distinctio numquam placeat harum quia dicta odit necessitatibus saepe, hic sapiente?                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quod nostrum sunt rerum eius, aliquam quia, explicabo modi reiciendis excepturi quibusdam cupiditate perferendis deserunt obcaecati repellendus veniam, laudantium mollitia veritatis!</p>
+            </div>
+            <div class="col-12 information">
+                <h2> Additional information </h2>
+                <div class="information">
+                <table class="woocommerce-product-attributes shop_attributes">
+                    <tbody>
+                        <tr class="woocommerce-product-attributes-item woocommerce-product-attributes-item--attribute_pa_pattern">
+                                <th class="woocommerce-product-attributes-item__label">Pattern</th>
+                                <td class="woocommerce-product-attributes-item__value"><p>Green, Brown</p></td>
+                        </tr>
+                        <tr class="woocommerce-product-attributes-item woocommerce-product-attributes-item--attribute_pa_size">
+                                <th class="woocommerce-product-attributes-item__label">Size</th>
+                                <td class="woocommerce-product-attributes-item__value"><p>S, M, L</p></td>
+                        </tr>
+                    </tbody>
+                </table>
+                </div>
+            </div>
+            <div class="col-12">
+                <h2> 2 reviews for Abstract Print Cotton Blouse </h2>
+                <div class="comment">
+                    <div class="user-image">
+                        <img src="./IMG/5.jpg" alt=""/>
+                        <div class="content-user">
+                            <p>Hady Mohamed</p>
+                            <p class="time">15/8/2020</p>
+                            <p class="description-comment">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur cum maiores ex officia tempora. Itaque unde architecto magni commodi rem. Quas aliquam aut nihil eius qui soluta sequi fuga. Odit.</p>
+                        </div>
+                    </div>
+                    <div class="user-image">
+                        <img src="./IMG/5.jpg" alt=""/>
+                        <div class="content-user">
+                            <p>Hady Mohamed</p>
+                            <p class="time">15/8/2020</p>
+                            <p class="description-comment">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur cum maiores ex officia tempora. Itaque unde architecto magni commodi rem. Quas aliquam aut nihil eius qui soluta sequi fuga. Odit.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 comment-start-user">
+                <span class="start-comment"> Add a review</span>
+            </div>
+            <div class="col-12 comment-start-user">
+                <form class="comment-here">
+                    <p><textarea placeholder = "Your review"></textarea></p>
+                    <div class="name-email">
+                        <div class="name" > <input type ="text"   placeholder = "Name"/></div>
+                        <div class="email" > <input type ="email" placeholder = "Email"/></div>
+                    </div>
+                    <button type="submit" class="sub-comment"> Submit </button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="refrend-cattgroy">
+        <h2> Related products </h2>
+                <div class="row">
+                    <div class="col-3 ref-all-content">
+                        <div class="img-ref-img">
+                            <img src="./IMG/5.jpg" alt="" />
+                        </div>
+                        <div class="content-ref">
+                            <p>Name of product</p>
+                            <p>40$</p>
+                        </div>
+                    </div>
+                    <div class="col-3 ref-all-content">
+                        <div class="img-ref-img">
+                            <img src="./IMG/6.jpg" alt="" />
+                        </div>
+                        <div class="content-ref">
+                            <p>Name of product</p>
+                            <p>40$</p>
+                        </div>
+                    </div>
+                    <div class="col-3 ref-all-content">
+                         <div class="img-ref-img">
+                            <img src="./IMG/7.jpg" alt="" />
+                        </div>
+                        <div class="content-ref">
+                            <p>Name of product</p>
+                            <p>40$</p>
+                        </div>
+                    </div>
+                    <div class="col-3 ref-all-content">
+                        <div class="img-ref-img">
+                            <img src="./IMG/7.jpg" alt="" />
+                        </div>
+                        <div class="content-ref">
+                            <p>Name of product</p>
+                            <p>40$</p>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
 </div>
