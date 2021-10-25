065d40e4787e59cfe0b1dcc0695917438c2b7f66
hadymohamedmohamedmorsy <hady812012@gmai.com>
2021-10-25 23:56:34 +0200

    add

--- Javascript-Files/product.js
+++ Javascript-Files/product.js
@@ -1,21 +1,23 @@
 let SecoundSection = document.querySelector('.Secound-Section .row');
 let setallvaluehere = "";
 
-fetch('./json/products.json')
+fetch('.API-Product.php')
   .then((response) => {
     return response.json();
   })
   .then((myJson) => {
-    myJson.Product.map(item => {
-        setallvaluehere = 
-        `<div class="${item.col} Products">
-            <img src=${item.IMG} alt="${item.alt}" />
-            <p>${item.name}</p>
-            <span>${item.price}</span>
-        </div>`;
-        SecoundSection.innerHTML += setallvaluehere;
+    // myJson.Product.map(item => {
+    //     setallvaluehere = 
+    //     `<div class="${item.col} Products">
+    //         <img src=${item.IMG} alt="${item.alt}" />
+    //         <p>${item.name}</p>
+    //         <span>${item.price}</span>
+    //     </div>`;
+    //     SecoundSection.innerHTML += setallvaluehere;
 
-    });
+    // });
+
+    console.log(myJson);
 
   });
   let clickfilter = document.querySelector('.click-filter');
