83477e048bba3e7f130410c7e5823be2530b6b40
hadymohamedmohamedmorsy <hady812012@gmai.com>
2021-10-25 23:49:40 +0200

    add

--- Website-Structure/index-Body.php
+++ Website-Structure/index-Body.php
@@ -23,3 +23,55 @@
         <span class="sr-only">Next</span>
     </a>
 </div>
+
+<div class="container">
+    <div class="choose-price">
+    <div class="row">
+        <div class="col-4 Image-choose-price">
+            <img src="./IMG/8.jpg" alt="8.jpg"/>
+        </div>
+        <div class="col-4 Image-choose-price">
+            <img src="./IMG/9.jpg" alt="9.jpg"/>
+        </div>
+        <div class="col-4">
+            <div class="Clearance-Sales">
+
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
+<div class="sec_row container-fluid"></div>
+<div class="sec-after-row container">
+    <div class="addrese-section">
+        <h2>Best Selling</h2>
+        <p>Bushwick blue bottle photo booth copper mug marfa craft beer pour-over swag subway tile organic cloud bread.</p>
+    </div>
+    <div class="best-seler">
+        <div class="row">
+            <div class="col-4 Image-choose-best">
+                <img src="./IMG/10.jpg" alt="8.jpg"/>
+                <p class="name-product"> Name product </p>
+                <p class="price"> 20$ </p>
+            </div>
+            <div class="col-4 Image-choose-best">
+                <img src="./IMG/11.jpg" alt="8.jpg"/>
+                <p class="name-product"> Name product </p>
+                <p class="price"> 20$ </p>
+            </div>
+            <div class="col-4 Image-choose-best">
+                <img src="./IMG/12.jpg" alt="8.jpg"/>
+                <p class="name-product"> Name product </p>
+                <p class="price"> 20$ </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="testemonial">
+    <h2>Testemonial</h2>
+    <div class="content-test container">
+        <div class="person-tes">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil explicabo labore facilis</p>
+        </div>
+    </div>
+</div>
