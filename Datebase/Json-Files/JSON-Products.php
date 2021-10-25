8c41e8a4ca5b2d6552656bfbcfd222073536c8b2
AbdulBaset R. Sayed <74756037+AbdulBasetRS@users.noreply.github.com>
2021-10-26 00:27:00 +0200

    Update

--- Datebase/Json-Files/JSON-Products.php
+++ Datebase/Json-Files/JSON-Products.php
@@ -1,6 +1,5 @@
 <?php 
     header('Content-Type: json');
-
     $Current_Date = date('Y/m/d');
     $Current_Time = date('h:i:s A');
     $Current_Date_And_Time = $Current_Date . ' ' . $Current_Time;
@@ -22,7 +21,6 @@
     $i = 0;
     while ($Rows = mysqli_fetch_assoc($Result_Products_Info)) {
         $Products_Details_JSON[] = $Rows;
-
         // Get Category Name
         $SQL_Category_Name = 'SELECT HA_C_L_Category_Name FROM ha_category_list WHERE HA_C_L_ID = "'.$Rows['Product_Category_ID'].'"';
         $Result_Category_Name = mysqli_query($Connection,$SQL_Category_Name);
@@ -31,11 +29,9 @@
         $SQL_Owner_User = 'SELECT HA_U_Username FROM ha_users WHERE HA_U_ID = "'.$Rows['Product_Owner_ID'].'"';
         $Result_Owner_User = mysqli_query($Connection,$SQL_Owner_User);
         $Row_Owner_User  = mysqli_fetch_array($Result_Owner_User, MYSQLI_ASSOC);
-
         // Insert Data Key And Value In Json Array
         $Products_Details_JSON[$i]['Product_Category_Name'] = $Row_Category_Name['HA_C_L_Category_Name'] ;
         $Products_Details_JSON[$i]['Product_Username_Created'] = $Row_Owner_User['HA_U_Username'] ;
-
         // Get Img Path
         $Path_Cover = 'IMG/Imges-Products/P_ID-' . $Rows['Product_ID'] . '/' . 'P_ID-' . $Rows['Product_ID'] . '-Cover/';
         $Path_Imgs = 'IMG/Imges-Products/P_ID-' . $Rows['Product_ID'] . '/' . 'P_ID-' . $Rows['Product_ID'] . '-IMGS/';
@@ -51,8 +47,6 @@
             $Products_Details_JSON[$i]['Product_Path_Imges'] =  $Path_Imgs ;
             $Products_Details_JSON[$i]['Product_Imges'] =  array_slice($IMG_Product,2) ;
         }
-
-        
         $i++;
     }
     echo json_encode($Products_Details_JSON,JSON_PRETTY_PRINT);
