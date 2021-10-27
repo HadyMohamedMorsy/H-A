d9386a8061ab20cb042d5d3fabc9db1a6d7fdd26
hadymohamedmohamedmorsy <hady812012@gmai.com>
2021-10-28 00:56:48 +0200

    add

--- Website-Structure/About-Us-Body.php
+++ Website-Structure/About-Us-Body.php
@@ -13,3 +13,125 @@
     </div>
 </div>
 <!-- header -->
+
+<div class="about-video">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 video">
+                <img src="./IMG/video.jpg" alt=""/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-play"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>
+            </div>
+            <div class="col-lg-6 content-video">
+                <h5 class="fancy-title">Our History</h5>
+                <h2>Setting Industry Standards</h2>
+                <p>Portland meggings chartreuse plaid palo santo, gluten-free ramps iPhone etsy salvia cray kombucha copper mug single-origin coffee.</p>
+                <div class="number-off-viewers">
+                    <div class="number">
+						<div class="count"> <span> 12346 </span></div>
+						<h4>Customers</h4>
+                    </div>
+                    <div class="content-number">
+                        <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="our-story">
+    <h5 class="fancy-title" style="text-align: center;">Est. 2021</h5>
+    <h2 style="text-align: center;">Our Story</h2>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, ullam facilis maiores iste velit aliquid doloribus nostrum maxime itaque nobis mollitia, optio, sint officia? Mollitia iusto fugit repellendus ullam asperiores?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error provident dolorem dolor. Vero dolore tempora fuga, sequi odio labore ad, sed illo minus ratione corrupti voluptatum blanditiis officiis neque non?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error provident dolorem dolor. Vero dolore tempora fuga, sequi odio labore ad, sed illo minus ratione corrupti voluptatum blanditiis officiis neque non?</p>
+                <p>Lorem, ipsum dolor sit amet consectetur</p>
+            </div>
+            <div class="col-lg-6">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, ullam facilis maiores iste velit aliquid doloribus nostrum maxime itaque nobis mollitia, optio, sint officia? Mollitia iusto fugit repellendus ullam asperiores?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error provident dolorem dolor. Vero dolore tempora fuga, sequi odio labore ad, sed illo minus ratione corrupti voluptatum blanditiis officiis neque non?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error provident dolorem dolor. Vero dolore tempora fuga, sequi odio labore ad, sed illo minus ratione corrupti voluptatum blanditiis officiis neque non?</p>
+                <p>Lorem, ipsum dolor sit amet consectetur</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="about-video">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 content-video">
+                <h5 class="fancy-title">OUR FUTURE</h5>
+                <h2>Growing Fast</h2>
+                <p>Thundercats art party edison bulb authentic roof party taiyaki synth gluten-free squid tumeric retro iPhone etsy.</p>
+                <div class="number-off-viewers">
+                    <div class="number">
+						<div class="count"> <span> 107 </span></div>
+						<h4>OFFICES</h4>
+                    </div>
+                    <div class="content-number">
+                        <p> Regional offices around the world.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 video">
+                <img src="./IMG/Future.jpg" alt=""/>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="core-value">
+    <h5 class="fancy-title">OUR FUTURE</h5>
+    <h2>Growing Fast</h2>
+    <p>Thundercats art party edison bulb authentic roof party taiyaki synth gluten-free squid tumeric retro iPhone etsy.</p>
+    <div class="container content-all">
+        <div class="row">
+            <div class="col-lg-4 ">
+                <span> <i class="far fa-trash-alt"></i></span>
+                <h4>Cruises & Water Tours</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur.</p>
+            </div>
+            <div class="col-lg-4">
+                <span><i class="fas fa-signal"></i></span>
+                <h4>Night Life</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur .</p>
+            </div>
+            <div class="col-lg-4">
+                <span><i class="fas fa-share-alt"></i></span>
+                <h4>Hiking</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur .</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="fonderes">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 content-founder">
+            <h5 class="fancy-title">FOUNDERS</h5>
+            <h2>Key People</h2>
+            <p>Yielding fowl their brought is own day place fruit creature our day lesser cant bring hath after.</p>
+            </div>
+            <div class="col-lg-3 content-person">
+                <img src="./IMG/first.jpg" alt=""/>
+                <h3>John Nicholson</h3>
+                <span>Developer</span>
+            </div>
+            <div class="col-lg-3 content-person">
+                <img src="./IMG/secoound.jpg" alt=""/>
+                <h3>Sandra Monroe</h3>
+                <span>Marketing</span>
+            </div>
+            <div class="col-lg-3 content-person">
+                <img src="./IMG/thired.jpg" alt=""/>
+                <h3>Denisse MacDonnell</h3>
+                <span>CEO</span>
+            </div>
+        </div>
+    </div>
+</div>
