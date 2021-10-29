191672208e70458dc5ee1e1fa8d3089ed77cdd4e
hadymohamedmohamedmorsy <hady812012@gmai.com>
2021-10-29 19:01:25 +0200

    add

--- Website-Structure/cart-body.php
+++ Website-Structure/cart-body.php
@@ -3,20 +3,108 @@
 </div>
 <div class="cheack-price container">
         <div class="list">
-            <div class="content-list">
-                <img src="./IMG/1.jpg" alt=""/>
-                <div class="content-price">
-                    <p> Cashmere Carpenter Beanie </p>
-                    <span>$39.00</span>
+            <div class="content-parent row">
+                <div class="content-grid col-lg-7">
+                    <div class="content-list">
+                        <img src="./IMG/1.jpg" alt=""/>
+                        <div class="content-price">
+                            <p> Cashmere Carpenter Beanie </p>
+                            <span>$39.00</span>
+                        </div>
+                        <div class="plus-min">
+                            <span>+</span>
+                            <span>1</span>
+                            <span>-</span>
+                        </div>
+                        <div class="Total">
+                            <p>$39.00</p>
+                            <span>X</span>
+                        </div>
+                    </div>
+                    <div class="content-list">
+                        <img src="./IMG/1.jpg" alt=""/>
+                        <div class="content-price">
+                            <p> Cashmere Carpenter Beanie </p>
+                            <span>$39.00</span>
+                        </div>
+                        <div class="plus-min">
+                            <span>+</span>
+                            <span>1</span>
+                            <span>-</span>
+                        </div>
+                        <div class="Total">
+                            <p>$39.00</p>
+                            <span>X</span>
+                        </div>
+                    </div>
+                    <form class="code-discound">
+                        <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="Coupon code">
+                        <button type="submit" class="button" name="apply_coupon" value="Apply coupon">Apply coupon</button>
+                    </form>
                 </div>
-                <div class="plus-min">
-                    <span>+</span>
-                    <span>1</span>
-                    <span>-</span>
+                <div class="cheackout-prent col-lg-5">
+                    <form class="payment">
+                        <div class="total"><h2>Cart totals</h2></div>
+                        <div class="total">
+                            <h2> substotal </h2>
+                            <span> $117.00 </span>
+                        </div>
+                        <div class="shipping">
+                            <div>
+                              <input type="radio" id="Free shipping" name="fav_language" value="Free shipping">
+                              <label for="html">Free shipping</label>
+                            </div>
+                            <div>
+                              <input type="radio" id="Flat Rate" name="fav_language" value="Flat Rate">
+                              <label for="html">Flat Rate</label>
+                            </div>
+                            <div>
+                                <span>Shipping to <span>CA</span></span>
+                            </div>
+                            <span class="price-subtotal">$10.00</span>
+                            <div class="changer-address">
+                                <span><i class="far fa-compass"></i><span>Change address</span> <span><i class="fas fa-chevron-down"></i></span></span>
+                                <div class="change">
+                                    <select class="form-control">
+                                        <option>Large select</option>
+                                    </select>
+                                    <select class="form-control">
+                                        <option>Default select</option>
+                                    </select>
+                                    <input type="text" name="fav_language" class="form-control" placeholder="city"/>
+                                    <input type="number" name="fav_language" class="form-control" placeholder="zip"/>
+                                    <button type="button"> Update </button>
+                                </div>
+                                <div class="total-all">
+                                    <h2> Total </h2>
+                                    <span> $117.00 </span>
+                                </div>
+                                <ul class="choose-payment">
+                                    <li class="wc_payment">
+                                        <input id="payment_method_cod" type="radio" class="input-radio" name="payment_method" value="cod" checked="checked" data-order_button_text="">
+                                        <label for="payment_method_cod">
+                                            Cash on delivery 	
+                                        </label>
+                                        <div class="payment_box payment_method_cod">
+                                            <p>Pay with cash upon delivery.</p>
+                                        </div>
+                                    </li>
+                                    <li class="wc_payment paybal-link">
+                                        <input id="payment_method_paypal" type="radio" class="input-radio" name="payment_method" value="paypal">
+                                        <label for="payment_method_paypal">
+                                            PayPal 
+                                        </label>
+                                        <img src="./IMG/paybal.jpg" alt="PayPal acceptance mark">
+                                        <a href="https://www.paypal.com/us/webapps/mpp/paypal-popup" class="about_paypal">What is PayPal?</a>	
+	                                </li>
+                                </ul>
+                                <div class="submit-price">
+                                    <button type="submit" class="form-control"> Place Order </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="cheackout-prent">
-                Hello
             </div>
         </div>
         <div class="list-form-confirm"></div>
