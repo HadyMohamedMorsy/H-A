ec77a87bba583fc15ff82bc60028c3a1ef165056
hadymohamedmohamedmorsy <hady812012@gmai.com>
2021-10-29 09:19:59 +0200

    add

---
+++ Website-Structure/cart-body.php
@@ -0,0 +1,23 @@
+<div class="cart-items">
+    <h1 class="page-title" itemprop="name headline">Cart</h1>
+</div>
+<div class="cheack-price container">
+        <div class="list">
+            <div class="content-list">
+                <img src="./IMG/1.jpg" alt=""/>
+                <div class="content-price">
+                    <p> Cashmere Carpenter Beanie </p>
+                    <span>$39.00</span>
+                </div>
+                <div class="plus-min">
+                    <span>+</span>
+                    <span>1</span>
+                    <span>-</span>
+                </div>
+            </div>
+            <div class="cheackout-prent">
+                Hello
+            </div>
+        </div>
+        <div class="list-form-confirm"></div>
+</div>
