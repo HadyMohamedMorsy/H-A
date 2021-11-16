bdb4affbf3a2f80769d5cf118fb9d7855e35fc1a
hadymohamedmohamedmorsy <hady812012@gmai.com>
2021-11-16 13:01:07 +0200

    add

--- shop.php
+++ shop.php
@@ -96,10 +96,45 @@ let protecte = "sorry u dont have Items";
                 if (XHR.status == 200) {
 
                     console.log(JSON.parse(Data));
-                    
+
                     ele.firstElementChild.innerHTML = `Add To Cart`;
+
+
                     cartaction.classList.add('actioncart');
-                    cartlisthere.classList.add('actioncartlist')
+
+
+                    cartlisthere.classList.add('actioncartlist');
+
+
+                    (Addtocart = function (Carts = []) {
+                        let protectCart = Carts.map((item)=>{
+                            return `
+                            <div class="list">
+                                <div class="product-list-cart">
+                                    <div class="img-product-cart">
+                                        <img src=${item.Image}>
+                                    </div>
+                                    <div class="content-product">
+                                        <h6> ${item.productName} </h6>
+                                        <span class="min"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-minus"><line x1="5" y1="12" x2="19" y2="12"></line></svg> </span> <span> ${item.HA_C_Qty} </span> <span class="plus"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg> </span>
+                                        <div class="cancel-cart cart-list-cancel">
+                                            <svg class="svg-inline--fa fa-times fa-w-11" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" data-fa-i2svg=""><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg><!-- <i class="fas fa-times"></i> -->
+                                        </div>
+                                        <div class="price-cart">
+                                            <span class="Number-format">${item.HA_C_Unit_Price}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            `;
+
+                        });
+
+                        List.innerHTML = protectCart.join("");
+
+                    })(JSON.parse(Data)|| protecte);
+
+
                 }
             }
         });
